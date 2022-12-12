92365fce652f3a06e86b6188db31de466db584cd
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-12-13 07:47:21 +0800

    Add DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ModelDemoDNCore.Models;
+
+namespace ModelDemoDNCore.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Item> Item { get;set;}
+    }
+}

0b12d5b64db7dc3d91aa93af3052529b3789679f
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-12-13 21:22:28 +0800

    Add index add Controller db

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ModelDemoDNCore.Data;
+using ModelDemoDNCore.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelDemoDNCore.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ItemController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList=_db.Item;
+            return View(objList);
+        }
+    }
+}

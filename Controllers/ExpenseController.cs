329f73552e0344ad51478805ae8487424e3fa4cb
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-12-24 11:11:04 +0800

    新增delete

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -40,5 +40,36 @@ namespace ModelDemoDNCore.Controllers
             }
             return View(obj);
         }
+        //get Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id ==null || id==0)
+            {
+                return NotFound();
+            }
+            var obj=_db.Expenses.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj=_db.Expenses.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+
+                _db.Expenses.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+        }
     }
 }
